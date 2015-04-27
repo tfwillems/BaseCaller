b961b6973a9f6fc13059eaf10052178eb5700a75
tfwillems <twillems@mit.edu>
2015-04-27 19:00:59 -0400

    Create file to read .cif illumina files

---
+++ io.cpp
@@ -0,0 +1,6 @@
+#include "io.h"
+
+void read_cif(std::string &input_file){
+  
+
+}
---
+++ io.h
@@ -0,0 +1,8 @@
+#ifndef IO_H_
+#define IO_H_
+
+#include <string>
+
+void read_cif(std::string &input_file);
+
+#endif
